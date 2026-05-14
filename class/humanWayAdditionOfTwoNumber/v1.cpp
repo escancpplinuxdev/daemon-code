d6c6f0c946c8dbfe979712f7e28257312184428e
escancpplinuxdev <escancpplinuxdev@gmail.com>
2026-05-14 20:57:59 +0530

    14052026

--- class/humanWayAdditionOfTwoNumber/v1.cpp
+++ class/humanWayAdditionOfTwoNumber/v1.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-//#include <algorithm>
+#include <algorithm>
 using namespace std;
 
 class Employee
@@ -134,8 +134,144 @@ int main()
 
 
 
+	cout<<"\npop_back demo\n";
+
+	v.pop_back();
+	printVector(v,"After pop_back()");
+	v.pop_back();
+	printVector(v,"After pop_back()");
+	v.pop_back();
+	printVector(v,"After pop_back()");
+	v.pop_back();
+	printVector(v,"After pop_back()");
+	v.pop_back();
+	printVector(v,"After pop_back()");
 
 
+	cout<<"\nreserve demo\n";
+
+	vector <int> reserveVector;
+
+	for(int i =1; i<16; i++)
+	{
+		reserveVector.push_back(i);
+	}
+
+	printVector(reserveVector,"Before reserveVector.reserve(10)");
+	reserveVector.reserve(10);
+
+	cout<<"Ater reserveVector.reserve(10) Reserved capacity = "<<reserveVector.capacity()<<endl;
+	printVector(reserveVector,"After reserveVector.reserve(10)");
+
+	cout<<"\nResize demo\n";
+	
+	reserveVector.resize(10);
+
+	cout<<"size = "<<reserveVector.size()<<endl;
+	printVector(reserveVector,"After resize Vector.size()");
+
+	cout<<"\nEmplace_back demo : \n";
+
+	vector<Employee>employees;
+
+	employees.emplace_back(101,"Rushi");
+	employees.emplace_back(102,"Amit");
+	
+	cout<<"employees : \n";
+
+	for(const auto & e : employees)
+	{
+		e.display();
+	}
+/*
+	for(int i =0; i<employees.size();i++)
+	{
+		cout<<" "<<employees[i][i];
+	}
+*/
+
+	cout<<"sort demo\n";
+	
+	vector<int>sortVector =
+	{
+		55,
+		11,
+		96,
+		99,
+		22,
+		60
+
+	};
+
+	printVector(sortVector,"sortVector = ");
+	sort(sortVector.begin(), sortVector.end());
+	
+	printVector(sortVector,"sortVector = ");
+	for(const auto & x : sortVector)
+	{
+		cout<< x <<" ";
+	}
+	cout<<endl;
+
+	for(int i = 0;i<sortVector.size();i++)
+	{
+		cout<<sortVector[i]<<" ";
+	}
+	cout<<endl;
+
+	cout<<"\nReverse demo\n";
+
+	reverse(sortVector.begin(),sortVector.end());
+
+	for(const auto &x : sortVector)
+	{
+		cout<< x <<" ";
+	}
+
+	printVector(sortVector,"Reverse sortVector");
+
+	cout<<endl;
+
+	cout<<"\nVector<string>demo"<<endl;
+
+	vector<string>names;
+
+	names.push_back("rushi");
+	names.push_back("ayushi");
+	names.push_back("mahhi");
+	names.push_back("rdesmoushi");
+	printVector(sortVector,"Stirng  names vector Vector");
+	reverse(sortVector.begin(),sortVector.end());
+
+	for(int i=0;i<names.size();i++)
+	{
+		cout<<names[i]<<" ";
+	}
+	cout<<endl;
+
+	cout<<"\nPointer invalidation demo\n";
+
+	vector<int>pointerVector;
+
+	pointerVector.push_back(10);
+	pointerVector.push_back(20);
+	pointerVector.push_back(30);
+	pointerVector.push_back(40);
+
+
+	int *ptr = &pointerVector[0];
+
+	cout<<"Before REallocation Address = "<<ptr<<endl;
+
+	for(int i = 0; i<10;i++)
+	{
+		pointerVector.push_back(i);
+	}
+	
+	cout<<"After Reallocation Address = "<<&pointerVector[0]<<endl;
+	cout<<"old allocated Address =	 "<<ptr<<endl;
+
+	cout<<"\nOld pointer may become dangling!"<<endl;
 
 
 	return 0;
